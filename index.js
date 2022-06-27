24b1b4b9b2ae259e668973b4431072f7b075f8aa
Adonis Kakoulidis <aklp08@gmail.com>
2022-06-28 00:34:31 +0200

    uriencode the branch name by default

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function main() {
         const appCenterToken = core.getInput('appcenter-token');
         const sourceBranch = core.getInput('source-branch');
         const projectId = core.getInput('project-id');
-        const targetBranch = core.getInput('target-branch');
+        const targetBranch = encodeURIComponent(core.getInput('target-branch'));
 
         const url = `https://api.appcenter.ms/v0.1/apps/${projectId}/branches/${targetBranch}/config`;
 
